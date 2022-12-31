1a6d71e70c1c8ddcddbf944cebd2d1418947b092
chemokita13 <chemokita13@gmail.com>
2022-12-31 19:48:07 +0100

    avoid __v:0 mongodb

--- src/notes/note.schema.ts
+++ src/notes/note.schema.ts
@@ -1,7 +1,10 @@
 import { Schema } from 'mongoose';
 
-export const NoteSchema = new Schema({
-  title: String,
-  content: String,
-  author: String,
-});
+export const NoteSchema = new Schema(
+  {
+    title: String,
+    content: String,
+    author: String,
+  },
+  { versionKey: false }, // avoid __v:0
+);
