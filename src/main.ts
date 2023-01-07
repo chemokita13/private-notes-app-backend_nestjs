6ee96166630e101f8e0d3086d39d05518f4f6bf6
chemokita13 <chemokita13@gmail.com>
2023-01-07 23:36:22 +0100

    origin cors setted

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({ credentials: true, origin: 'http://localhost:4000' });
   await app.listen(3000);
 }
 bootstrap();
